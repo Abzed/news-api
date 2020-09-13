9164c9e5f33c1c4ac40d2a74eef19515de5616dd
Abzed <ibraabzed@gmail.com>
2020-09-13 12:23:48 +0300

    add models for news source and news article

---
+++ app/models.py
@@ -0,0 +1,17 @@
+class News_source:
+    def __init__(self,id, name, description, category, language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.language = language
+    
+    
+class News_article:
+    def __init__(self,author, title, description, url_link, url_image, date_publushed):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url_link = url_link
+        self.url_image = url_image
+        self.date_publushed = date_publushed
