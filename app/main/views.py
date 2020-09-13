66645598e8c2df83f3a174089969dcdf0d8ded5d
Abzed <ibraabzed@gmail.com>
2020-09-13 20:32:46 +0300

    add different news categories

--- app/main/views.py
+++ app/main/views.py
@@ -6,4 +6,9 @@ from ..models import News_source
 @main.route('/')
 def index():
     general_news = get_sources('general')
-    return render_template('index.html', general=general_news)
+    sports_news = get_sources('sports')
+    technology_news = get_sources('technology')
+    entertainment_news = get_sources('entertainment')
+    business_news = get_sources('business')
+    science_news = get_sources('science')
+    return render_template('index.html', general=general_news,sports=sports_news,technology=technology_news,entertainment=entertainment_news,business=business_news,science=science_news)
