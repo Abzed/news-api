66828f14971ca996ff9c5f469a0840b932f75e3c
Abzed <ibraabzed@gmail.com>
2020-09-13 10:24:41 +0300

    import config options to app

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,12 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    app.config.from_object(config_options[config_name])
+    
+    bootstrap.init_app(app)
+    
+    return app
