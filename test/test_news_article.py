19cc11f066fc6b9e058f969f8a933d6abc3409b5
Abzed <ibraabzed@gmail.com>
2020-09-13 13:25:24 +0300

    add test case for news articles

---
+++ test/test_news_article.py
@@ -0,0 +1,12 @@
+import unittest
+from app.models import News_article
+
+class NewsArticleTest(unittest.TestCase):
+    def setUp(self):
+        self.new_news_article = News_article('abz-news', 'Abzed gets to Moringa core', 'As he is ready to complete his studies he faces Python as a tough opponent', 'https:abzed.abzed/moringachronicals', 'https://abzed.image', '2020-09-13 13:23')
+        
+    def test_instance(self):
+        self.assertEqual(isinstance(self.new_news_article))
+        
+if __name__ == '__main__':
+    unittest.main()
