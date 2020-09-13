37c15f4a1d514d0f42d7a37ef15031aaadb8677c
Abzed <ibraabzed@gmail.com>
2020-09-13 14:11:11 +0300

    add function to fetch news sources

--- app/request.py
+++ app/request.py
@@ -1,4 +1,5 @@
 import urllib.request, json
+from .models import News_source, News_article
 
 api_key = None
 
@@ -8,4 +9,20 @@ def configure_request(app):
     global api_key, base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_BASE_URL']
+    
+def get_news_source(sources):
+    get_news_sources_url = base_url.format(sources,api_key)
+    
+    with urllib.request.urlopen(get_news_sources_url) as url:
+        get_news_sources_data = url.read()
+        get_news_sources_response = json.loads(get_news_sources_data)
+        
+        news_sources_result = None
+        
+        if get_news_sources_response['results']:
+            news_source_list = get_news_sources_response['results']
+            news_sources_result = process_results(news_source_list)
+            
+    return news_sources_result
+
     
